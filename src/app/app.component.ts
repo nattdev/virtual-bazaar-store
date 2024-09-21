8601a7ee3120bddc751748f5da9e8b456ef66f14
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-09-20 22:48:55 -0500

    Implement flowbite and tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'virtual-bazaar-store';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
+
