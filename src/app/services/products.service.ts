6c53129c231078d6277ae7a14a4abc25bbed8938
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-09-22 17:24:05 -0500

    Create products service to get the data form the api

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  private apiUrl = 'https://fakestoreapi.com/products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<any> {
+      const products = this.http.get(this.apiUrl);
+      return products;
+  }
+}
