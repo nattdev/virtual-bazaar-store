c05ef314159ff2c1bcb2b250229bebe8469f5db3
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-09-22 19:40:56 -0500

    Add router path to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { ShopComponent } from './pages/shop/shop.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
     { path: 'shop', component: ShopComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
 ];
