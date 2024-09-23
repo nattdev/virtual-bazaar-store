c02a1812ae5f312eb144ee94f15cb99f0d4328a2
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-09-23 00:06:11 -0500

    Add arrivals section to the home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SlicePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  products: any[] = [];
 
+  constructor(private productsServices: ProductsService) {}
+
+  ngOnInit() {
+    this.logData();
+  }
+
+ logData() {
+    this.productsServices.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
 }
