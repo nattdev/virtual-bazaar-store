c9fd3c7c393ac39b4aadb30aa2b332bd49267d29
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-09-24 00:28:09 -0500

    Add path of categories to the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'shop', component: ShopComponent},
+    { path: 'shop/:category', component: ShopComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home' }
 ];
