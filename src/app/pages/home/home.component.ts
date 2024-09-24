bae15956f41572937876fba4db906a251e9d45a7
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-09-24 01:07:16 -0500

    Refactor Home by extracting Our-Services into a separate component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from "../../components/carousel/carousel.component";
 import { ArrivalsComponent } from "../../components/arrivals/arrivals.component";
+import { OurServicesComponent } from "../../components/our-services/our-services.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, ArrivalsComponent],
+  imports: [CarouselComponent, ArrivalsComponent, OurServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
