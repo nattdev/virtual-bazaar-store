842383eccd13c1072f3745e8a1be101381839b77
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-09-30 20:50:32 -0500

    Added route for product details with id parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { ShopComponent } from './pages/shop/shop.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'shop', component: ShopComponent},
     { path: 'shop/:category', component: ShopComponent},
+    { path: 'product/:id', component: ProductDetailComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home' }
 ];
