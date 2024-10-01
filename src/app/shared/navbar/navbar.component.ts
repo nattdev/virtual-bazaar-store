cde630a8db2cbe9beaa982d4d28ebde4ade10548
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-09-30 20:02:58 -0500

    Implement toggleHidden functionality to show/hide navbar items on mobile

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+isHidden = true;
+
+toggleHidden() {
+  this.isHidden = !this.isHidden;
+  console.log(this.isHidden);
+}
 
 }
